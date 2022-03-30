22c3cc0097bd38264864dd70629c8c4ee71e0cb4
Cassiano Caron <cassianocaron@hotmail.com>
2022-03-30 10:39:55 -0300

    Refactor code

--- P004/p004.py
+++ P004/p004.py
@@ -1,23 +1,32 @@
-""" Find the largest palindrome made from the product of two 3-digit numbers """
+from time import process_time
 
 
 def main():
+    start_time = process_time()
+
     largest_pal = 0
-    p1 = 999
-    while p1 >= 100:
-        p2 = 999
-        while p2 >= p1:
-            if (p1 * p2) <= largest_pal:
+    n1 = 999
+
+    while n1 >= 100:
+        n2 = 999
+
+        while n2 >= n1:
+            if (n1 * n2) <= largest_pal:
                 break
-            if is_palindrome(p1 * p2):
-                largest_pal = (p1 * p2)
-                largest_p1 = p1
-                largest_p2 = p2
-            p2 -= 1
-        if (largest_pal / 999) >= p1:
+
+            if is_palindrome(n1 * n2):
+                largest_pal = (n1 * n2)
+                largest_n1 = n1
+                largest_n2 = n2
+            n2 -= 1
+
+        if (largest_pal / 999) >= n1:
             break
-        p1 -= 1
-    print(f"{largest_p1} * {largest_p2} = {largest_pal}")
+        n1 -= 1
+
+    print(f"{largest_n1} * {largest_n2} = {largest_pal}")
+
+    print(f"\nTook {round(process_time() - start_time, 2)} seconds to run")
 
 
 def is_palindrome(n):
