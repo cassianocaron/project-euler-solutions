26b57e4ae17786d1048c842858701c057c1554ce
Cassiano Caron <cassianocaron@hotmail.com>
2022-03-30 09:50:36 -0300

    Refactor code

--- P002/p002.py
+++ P002/p002.py
@@ -1,22 +1,31 @@
-""" By considering the terms in the Fibonacci sequence whose values
-do not exceed four million, find the sum of the even-valued terms """
+from time import process_time
 
 
-MAX_TERM_VALUE = 4000000
+# Four million
+MAX_TERM_VALUE = 4E6
+
 
 def main():
-    result = 0
+    start_time = process_time()
+
+    even_nums = get_even_fibonacci_nums()
+    print(sum(even_nums))
+
+    print(f"\nTook {round(process_time() - start_time, 2)} seconds to run")
+
+
+def get_even_fibonacci_nums():
+    even_nums = []
     curr_term = 1
     next_term = 2
 
     while curr_term < MAX_TERM_VALUE:
         if curr_term % 2 == 0:
-            result += curr_term
-        # print(curr_term)
+            even_nums.append(curr_term)
         term_after_next = curr_term + next_term
         curr_term = next_term
         next_term = term_after_next
-    print(result)
+    return even_nums
 
 
 if __name__ == '__main__':
