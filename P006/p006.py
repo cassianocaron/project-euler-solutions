497b576db61d78a35ee3e288207bd13c8e18def1
Cassiano Caron <cassianocaron@hotmail.com>
2022-06-08 09:17:01 -0300

    Add README.md

--- P006/p006.py
+++ P006/p006.py
@@ -1,13 +1,18 @@
-""" Find the difference between the sum of the squares of the first 100 natural numbers and the square of the sum """
-
+from time import process_time
 
 LIMIT = 100
 
+
 def main():
+    start_time = process_time()
+
     sum_n = LIMIT * (LIMIT + 1) / 2
     sum_sq = (2 * LIMIT + 1) * (LIMIT + 1) * (LIMIT / 6)
     print(int(sum_n**2 - sum_sq))
 
-    
+    elapsed_time = process_time() - start_time
+    print(f"Execution time: {round(elapsed_time, 2)} seconds")
+
+
 if __name__ == '__main__':
     main()
