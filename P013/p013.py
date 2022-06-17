d5ed3bc31892e5472856232d5669a48dd8c9fc54
Cassiano Caron <cassianocaron@hotmail.com>
2022-06-17 09:09:33 -0300

    Add README.md

--- P013/p013.py
+++ P013/p013.py
@@ -1,7 +1,3 @@
-"""
-Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
-"""
-
 from time import process_time
 
 
@@ -9,13 +5,12 @@ def main():
     start_time = process_time()
 
     with open("numbers.txt", "r") as numbers:
-        num_array = []
-        for number in numbers:
-            num_array.append(int(number.strip("\n")))
+        num_array = [int(row.strip()) for row in numbers]
 
     print(str(sum(num_array))[:10])
 
-    print(f"\nTook {round(process_time() - start_time, 2)} seconds to run")
+    elapsed_time = process_time() - start_time
+    print(f"Execution time: {round(elapsed_time, 2)} seconds")
 
 
 if __name__ == '__main__':
